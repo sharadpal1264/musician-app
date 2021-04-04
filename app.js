e14dd3b071d07f28c4b31c56655e8bcf670f005f
sharadpal1264 <sharadpalv6@gmail.com>
2021-04-04 16:47:49 +0530

    seconf commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const musicianRoutes = require('./routes/musician');
 const app = express();
 const port = process.env.PORT || 3001;
 
-// include routes
+// include routessss
 app.use('/musician', musicianRoutes);
 
 app.use(express.static('public'));
