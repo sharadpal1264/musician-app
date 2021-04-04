0ae5738ed3692cddd57cfce4ebf1e65ec12135dd
sharadpal1264 <sharadpalv6@gmail.com>
2021-04-04 19:47:35 +0530

    commit chage title

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero is-primary">
       <div className="hero-body">
         <div className="container">
-          Musician App
+          Saloni Musician App
         </div>
       </div>
     </section>
